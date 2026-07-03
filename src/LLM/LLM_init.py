0bb6ca2149be1dfc9ccad0af2addb2dcda809b8c
Devansh Sharma <139527904+Devansh-react@users.noreply.github.com>
2026-07-03 13:10:54 +0530

    Updated the LLM

--- src/LLM/LLM_init.py
+++ src/LLM/LLM_init.py
@@ -1,27 +1,12 @@
-from langchain_groq import ChatGroq
+from langchain_mistralai import ChatMistralAI
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-if not os.getenv("GROQ_API_KEY"):
-    raise RuntimeError("GROQ_API_KEY not set in environment")
-
 if not os.getenv("MISTRAL_API_KEY"):
     raise RuntimeError("MISTRAL_API_KEY not set in environment")
 
-
-# def LLm_init(temperature: float = 0.1):
-#     model = ChatGroq(
-#         model="llama-3.3-70b-versatile",
-#         temperature=temperature,
-#         max_tokens=None,
-#         timeout=25,
-#         max_retries=1,
-#     )
-#     return 
-
-from langchain_mistralai import ChatMistralAI
 
 def LLm_init(temperature: float = 0.1):
     return ChatMistralAI(
